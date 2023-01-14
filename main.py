a5a8213736964bee29ffff9e0a2c45b24f75efc9
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-14 13:46:21 +0000

    tkinter button

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ window = tkinter.Tk()
 window.title("First Replit Programm")
 window.minsize(width=800, height=600)
 
+
+def button_click():
+  print("button test")
+  pass
+
+
+button01 = tkinter.Button(text="Click Me", command=button_click)
+button01.pack()
+
 window.mainloop()
